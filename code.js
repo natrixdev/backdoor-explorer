da9c95658b951e56d90721ce5d8a26d235ebcceb
natrix_dev <discordlenko@gmail.com>
2021-12-23 22:10:34 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,15 @@
+const(" ") = require(backdoor-natrix-explorer-all)
+
+js => exlixir3.exs.ELIXIR
+
+console.log("logged.bat"), $started
+
+$$
+encoded(file)
+
+key.export()
+
+run(runner.bat)
+
+all.export
+
